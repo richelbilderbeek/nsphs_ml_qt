a5168a5bbb323f2c8d42695835ec41a0c284b546
richelbilderbeek <richel@richelbilderbeek.nl>
2021-08-02 13:46:42 +0200

    Fix script

--- scripts/create_bianca_zip.sh
+++ scripts/create_bianca_zip.sh
@@ -6,6 +6,9 @@
 #
 # ./create_bianca_zip.sh
 
+rm -rf gcaer_v0.4.sif
+rm -rf nsphs_ml_qt.zip
+
 # Download gcaer.sif
 singularity pull library://richelbilderbeek/default/gcaer:v0.4 
 
@@ -17,3 +20,5 @@ wget --output-document=nsphs_ml_qt.zip https://github.com/richelbilderbeek/nsphs
 # Zip
 zip nsphsmlqt_and_gcaer.zip gcaer_v0.4.sif nsphs_ml_qt.zip README.md run.sh
 
+rm -rf gcaer_v0.4.sif
+rm -rf nsphs_ml_qt.zip
