23dcb42fd4094a47f11aa0a1bcd3984765b97a09
richelbilderbeek <richel@richelbilderbeek.nl>
2021-08-02 09:31:30 +0200

    Can create Bianca zip

--- scripts/create_bianca_zip.sh
+++ scripts/create_bianca_zip.sh
@@ -7,10 +7,11 @@
 # ./create_bianca_zip.sh
 
 # Download gcaer.sif
-singularity pull library://richelbilderbeek/default/gcaer:v0.1
+singularity pull library://richelbilderbeek/default/gcaer:v0.4 
 
 # Download this repo
-wget https://github.com/richelbilderbeek/nsphs_ml_qt/releases/download/v0.1/nsphs_ml_qt-v0.1.tar.gz
+wget --output-document=nsphs_ml_qt.zip https://github.com/richelbilderbeek/nsphs_ml_qt/archive/v0.1.zip 
 
 # Zip
-zip nsphsmlqt.zip nsphs_ml_qt-v0.1.tar.gz gcaer.sif
+zip nsphsmlqt_and_gcaer.zip gcaer_v0.4.sif nsphs_ml_qt.zip create_bianca_zip.sh
+
