fdc14651a0ac33daff3c37245658b4f709204621
richelbilderbeek <richel@richelbilderbeek.nl>
2021-08-23 11:23:48 +0200

    Fix script

--- scripts/1_unzip.sh
+++ scripts/1_unzip.sh
@@ -4,10 +4,9 @@
 #
 # Usage: 
 #
-#   ./unzip.sh
-#   sbatch -A snic2021-22-624 unzip.sh
-#   sbatch -A sens2021565 unzip.sh
-#
+#   ./1_unzip.sh
+#   sbatch -A snic2021-22-624 1_unzip.sh
+#   sbatch -A sens2021565 1_unzip.sh
 #
 #SBATCH --time=1:00:00
 #SBATCH --partition core
@@ -15,5 +14,4 @@
 #SBATCH --mem=1G
 #SBATCH --job-name=1_unzip
 #SBATCH --output=1_unzip.log
-unzip nsphs_ml_qt.zip
-
+unzip nsphsmlqt_and_gcaer.zip
