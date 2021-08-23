541e5b606f17bf3f101e092f947bf5bbff4d39a3
richelbilderbeek <richel@richelbilderbeek.nl>
2021-08-23 11:43:38 +0200

    Less time, less memory

--- scripts/2_sbatch_run.sh
+++ scripts/2_sbatch_run.sh
@@ -9,10 +9,10 @@
 #   sbatch -A sens2021565 run.sh
 #
 #
-#SBATCH --time=1:00:00
+#SBATCH --time=0:01:00
 #SBATCH --partition core
 #SBATCH --ntasks 1
-#SBATCH --mem=1G
+##SBATCH --mem=1G
 #SBATCH --job-name=2_run
 #SBATCH --output=2_run.log
 cat nsphs_ml_qt-master/scripts/do_experiment.R | ./gcaer_v0.4.sif
