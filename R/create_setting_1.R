203013a8ecacc2333d4bc438655d072cae048d4a
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-17 15:05:19 +0100

    Start of setting 1

---
+++ R/create_setting_1.R
@@ -0,0 +1,77 @@
+#' Create setting 1
+#'
+#' * Date: 2022-01-17
+#' * Goal: do the simplest useful simulation possible
+#' * Hypothesis: on the dimensionality reduction plots,
+#'   there will be three points.
+#'   With 'points' I really mean points, i.e. not clusters,
+#'   as there are exactly 3 genotypes
+#' * n = 1000
+#'
+#' Genotype|Phenotype|Allele frequency|Population|Superpopulation
+#' --------|---------|----------------|----------|---------------
+#' AA      |0        |25%             |A         |Global
+#' AC      |50       |50%             |B         |Global
+#' CC      |100      |25%             |C         |Global
+#'
+#' @return a \link{list} with the following elements:
+#'   * `bed_filename`: contains path to the `.bed` file
+#'   * `bim_filename`: contains path to the `.bim` file
+#'   * `fam_filename`: contains path to the `.fam` file
+#'   * `phe_filename`: contains path to the `.phe` file
+#'   * `labels_filename`: contains path to the file with the labels
+create_setting_1 <- function() {
+  # Code from 'gcaer/scripts/issue_2.R',
+  #
+  # Output must be in PLINK binary format
+  #
+  # Simulate data in PLINK1 text format
+  set.seed(1)
+  n_individuals <- 1000 # as NSPHS
+  assoc_qt_data <- plinkr::create_demo_assoc_qt_data(
+    n_individuals = n_individuals,
+    traits = plinkr::create_additive_trait(
+      mafs = 0.499999,
+      base_phenotype_value = 0,
+      phenotype_increase = 50
+    )
+  )
+
+  # Set populations
+  is_a <- assoc_qt_data$phenotype_data$phe_table$additive == 0
+  is_b <- assoc_qt_data$phenotype_data$phe_table$additive == 50
+  is_c <- assoc_qt_data$phenotype_data$phe_table$additive == 100
+  testthat::expect_equal(sum(is_a), 242)
+  testthat::expect_equal(sum(is_b), 516)
+  testthat::expect_equal(sum(is_c), 242)
+  testthat::expect_equal(sum(is_a) + sum(is_b) + sum(is_c), n_individuals)
+  assoc_qt_data$phenotype_data$phe_table$FID <- "Global"
+  assoc_qt_data$phenotype_data$phe_table$IID[is_a] <- "A"
+  assoc_qt_data$phenotype_data$phe_table$IID[is_b] <- "B"
+  assoc_qt_data$phenotype_data$phe_table$IID[is_c] <- "C"
+
+  assoc_qt_data$data <- plinkr::convert_plink_text_data_to_plink_bin_data(
+    plink_text_data = assoc_qt_data$data
+  )
+  base_input_filename <- "~/nsphs_nl_qt_setting_1"
+  filenames <- plinkr::save_plink_bin_data(
+    plink_bin_data = assoc_qt_data$data,
+    base_input_filename = base_input_filename
+  )
+  phe_filename <- paste0(base_input_filename, ".phe")
+  labels_filename <- paste0(base_input_filename, "_labels.csv")
+  plinkr::save_phe_table(
+    phe_table = assoc_qt_data$phenotype_data$phe_table,
+    phe_filename = phe_filename
+  )
+  filenames$phe_filename <- phe_filename
+  labels <- tibble::tribble(
+    ~super_population, ~population,
+    "Global", "A",
+    "Global", "B",
+    "Global", "C"
+  )
+  readr::write_csv(x = labels, file = labels_filename)
+  filenames$labels_filename <- labels_filename
+  filenames
+}
---
+++ tests/testthat/test-create_setting_1.R
@@ -0,0 +1,22 @@
+test_that("use", {
+  filenames <- create_setting_1()
+  base_input_filename <- tools::file_path_sans_ext(filenames$bed_filename)
+  plink_bin_data <- plinkr::read_plink_bin_data(base_input_filename = base_input_filename)
+  n_snps <- 1
+  n_individuals <- 1000
+  expect_equal(n_snps, nrow(plink_bin_data$bed_table))
+  expect_equal(n_individuals, ncol(plink_bin_data$bed_table))
+  expect_equal(n_snps, nrow(plink_bin_data$bim_table))
+  expect_equal(n_individuals, nrow(plink_bin_data$fam_table))
+
+  labels <- readr::read_csv(
+    filenames$labels_filename,
+    show_col_types = FALSE
+  )
+  expect_true(all(labels$super_population == "Global"))
+  expect_true(all(labels$population %in% LETTERS[1:3]))
+
+  skip("WIP")
+  expect_silent(plinkr::read_plink_phe_file(filenames$phe_filename))
+  phe_table <- plinkr::read_plink_phe_file(filenames$phe_filename)
+})
