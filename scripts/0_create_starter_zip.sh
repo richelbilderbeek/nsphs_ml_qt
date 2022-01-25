237e617a9d3d3bc5e1042676063c1a358ca4f70e
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-25 08:25:37 +0100

    Script must run locally

--- scripts/0_create_starter_zip.sh
+++ scripts/0_create_starter_zip.sh
@@ -8,5 +8,13 @@
 #   ./0_create_starter_zip.sh
 #   ./scripts/0_create_starter_zip.sh
 #
+
+if [[ $HOSTNAME != "N141CU" ]]; then
+  echo "Error: this script must run locally"
+  echo "HOSTNAME: $HOSTNAME"
+  exit 42
+fi
+
+
 zip -r --must-match 0_starter_zip.zip ~/GitHubs/gcaer/gcaer.sif ~/GitHubs/nsphs_ml_qt/scripts
 
