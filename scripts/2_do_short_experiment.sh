80353924abf0cf2c00fcbccd72ae0d935d934882
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-25 11:14:21 +0100

    Zip correctly

--- scripts/2_do_short_experiment.sh
+++ scripts/2_do_short_experiment.sh
@@ -4,8 +4,7 @@
 #
 # Usage:
 #
-#   ./run.sh
-#   sbatch 2_do_short_experiment.sh
+#   sbatch nsphs_ml_qt/scripts/2_do_short_experiment.sh
 #
 #SBATCH -A sens2021565
 #SBATCH --time=0:01:00
@@ -14,5 +13,5 @@
 #SBATCH --mem=1G
 #SBATCH --job-name=2_do_short_experiment
 #SBATCH --output=2_do_short_experiment.log
-cat 2_do_short_experiment.R | ./gcaer.sif
+cat nsphs_ml_qt/scripts/2_do_short_experiment.R | gcaer/gcaer.sif
 
