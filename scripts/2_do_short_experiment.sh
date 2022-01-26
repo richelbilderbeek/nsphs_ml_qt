8c383096a4e7a2b9aab1673f8e572955bc4dcb47
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-26 12:24:43 +0100

    Run 60 mins on Bianca

--- scripts/2_do_short_experiment.sh
+++ scripts/2_do_short_experiment.sh
@@ -7,7 +7,7 @@
 #   sbatch nsphs_ml_qt/scripts/2_do_short_experiment.sh
 #
 #SBATCH -A sens2021565
-#SBATCH --time=0:01:00
+#SBATCH --time=1:00:00
 #SBATCH --partition core
 #SBATCH --ntasks 1
 #SBATCH --mem=1G
--- scripts/3_run_setting_1.sh
+++ scripts/3_run_setting_1.sh
@@ -7,7 +7,7 @@
 #   sbatch nsphs_ml_qt/scripts/3_run_setting_1.sh
 #
 #SBATCH -A sens2021565
-#SBATCH --time=0:01:00
+#SBATCH --time=1:00:00
 #SBATCH --partition core
 #SBATCH --ntasks 1
 #SBATCH --mem=1G
