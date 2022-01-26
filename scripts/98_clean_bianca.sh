bbebb76a17a5344f5187dbd469072937e405ecd5
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-26 12:27:34 +0100

    Clean better

--- scripts/98_clean_bianca.sh
+++ scripts/98_clean_bianca.sh
@@ -6,7 +6,13 @@
 #
 #   ./nsphs_ml_qt/scripts/98_clean_bianca.sh
 #
-rm -f richel-sens2021565/0_starter_zip.zip.zip
-rm -f README.md
+
+# Path at Bianca
+rm -f richel-sens2021565/0_starter_zip.zip
+
+# Path at Rackham
+rm -f 0_starter_zip.zip
+
+rm -f README.md *.log *.png *.sh
 rm -rf gcaer  GenoCAE  nsphs_ml_qt
 
