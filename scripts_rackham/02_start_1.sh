81d75ec93323dc43816a98e37dca7cbb63cb6c11
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-02 14:29:22 +0100

    Can start experiment 1 from '02_start_1.sh'

---
+++ scripts_rackham/02_start_1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Do the full flow for experiment one
+#
+# Usage: 
+#
+#   ./nsphs_ml_qt/scripts_rackham/02_start_1.sh
+#   sbatch ./nsphs_ml_qt/scripts_rackham/02_start_1.sh
+#
+# You can without moral concerns run this script without 'sbatch',
+# as all it does is 'sbatch'-ing other scripts
+#
+#SBATCH -A snic2021-22-624
+#SBATCH --time=0:05:00
+#SBATCH --partition core
+#SBATCH --ntasks 1
+#SBATCH -C usage_mail
+# From https://www.uppmax.uu.se/support/user-guides/slurm-user-guide
+# Be light first
+# Could do, for 256GB: -C mem256GB
+# Could do, for 1TB: -C mem1TB
+#SBATCH --mem=16G
+#SBATCH --job-name=02_start_1
+#SBATCH --output=02_start_1.log
+
+echo "Running on computer with HOSTNAME: $HOSTNAME"
+echo "Running at location $(pwd)"
+
+jobid_10=$(sbatch                                ./nsphs_ml_qt/scripts_rackham/10_create_dataset_1.sh      | cut -d ' ' -f 4)
+jobid_11=$(sbatch --dependency=afterok:$jobid_10 ./nsphs_ml_qt/scripts_rackham/11_train_on_dataset_1.sh    | cut -d ' ' -f 4)
+jobid_12=$(sbatch --dependency=afterok:$jobid_10 ./nsphs_ml_qt/scripts_rackham/12_project_on_dataset_1.sh  | cut -d ' ' -f 4)
+jobid_13=$(sbatch --dependency=afterok:$jobid_10 ./nsphs_ml_qt/scripts_rackham/13_plot_on_dataset_1.sh     | cut -d ' ' -f 4)
+jobid_14=$(sbatch --dependency=afterok:$jobid_10 ./nsphs_ml_qt/scripts_rackham/14_animate_on_dataset_1.sh  | cut -d ' ' -f 4)
+jobid_15=$(sbatch --dependency=afterok:$jobid_10 ./nsphs_ml_qt/scripts_rackham/15_evaluate_on_dataset_1.sh | cut -d ' ' -f 4)
+
--- scripts_rackham/10_create_dataset_1.sh
+++ scripts_rackham/10_create_dataset_1.sh
@@ -8,9 +8,21 @@
 #   sbatch ./nsphs_ml_qt/scripts_rackham/10_create_dataset_1.sh
 #
 #
+#SBATCH -A snic2021-22-624
+#SBATCH --time=1:00:00
+#SBATCH --partition core
+#SBATCH --ntasks 1
+#SBATCH -C usage_mail
+# From https://www.uppmax.uu.se/support/user-guides/slurm-user-guide
+# Be light first
+# Could do, for 256GB: -C mem256GB
+# Could do, for 1TB: -C mem1TB
+#SBATCH --mem=16G
+#SBATCH --job-name=10_create_dataset_1
+#SBATCH --output=10_create_dataset_1.log
+
 echo "Running on computer with HOSTNAME: $HOSTNAME"
 echo "Running at location $(pwd)"
 
 Rscript nsphs_ml_qt/scripts_rackham/10_create_dataset_1.R
-
 
