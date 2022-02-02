9885a886cf042f918d462979784d53490ba36d20
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-02 09:15:35 +0100

    Create sim dataset 1, fix #1

---
+++ scripts_rackham/10_create_dataset_1.R
@@ -0,0 +1,13 @@
+#
+# Create simulated data
+#
+# Usage:
+#
+#   Rscript nsphs_ml_qt/scripts_rackham/10_create_dataset_1.R
+#
+
+gcaer::create_gcae_input_files_1(
+  base_input_filename = "nsphs_ml_qt/sim_data_1/sim_data_1"
+)
+
+
---
+++ scripts_rackham/10_create_dataset_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Create simulated dataset 1
+#
+# Usage: 
+#
+#   ./nsphs_ml_qt/scripts_rackham/10_create_dataset_1.sh
+#   sbatch ./nsphs_ml_qt/scripts_rackham/10_create_dataset_1.sh
+#
+#
+Rscript nsphs_ml_qt/scripts_rackham/10_create_dataset_1.R
+
+
