279c56fb634cc7966804ade7e21d71ff1a71e707
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-09 16:02:28 +0100

    Fix script

--- scripts_rackham/10_create_dataset_1.R
+++ scripts_rackham/10_create_dataset_1.R
@@ -6,6 +6,9 @@
 #   Rscript nsphs_ml_qt/scripts_rackham/10_create_dataset_1.R
 #
 args <- commandArgs(trailingOnly = TRUE)
+if (1 == 2) {
+  args <- c("1", "2", "3", "4")
+}
 
 if (length(args) != 4) {
   stop(
@@ -22,12 +25,15 @@ if (length(args) != 4) {
 }
 
 base_input_filename <- args[1]
-n_individuals <- args[2]
-n_traits <- args[3]
-n_snps_per_trait <- args[4]
+n_individuals <- as.numeric(args[2])
+n_traits <- as.numeric(args[3])
+n_snps_per_trait <- as.numeric(args[4])
 
 
 message("base_input_filename: ", base_input_filename)
+message("n_individuals: ", n_individuals)
+message("n_traits: ", n_traits)
+message("n_snps_per_trait: ", n_snps_per_trait)
 
 is_on_gha <- function() {
   Sys.getenv("GITHUB_ACTIONS") != ""
