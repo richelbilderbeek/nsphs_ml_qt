398aa82b4c90a31288831e645413d769e695a23f
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-28 10:23:24 +0100

    Clean better

--- scripts_bianca/98_clean_bianca.sh
+++ scripts_bianca/98_clean_bianca.sh
@@ -10,8 +10,8 @@
 # Note that the first is a copy of the second, as copied by
 # '00_create_starter_zip.sh'
 #
-rm -f richel-sens2021565/nsphs_ml_qt_starter_zip.zip
-rm -rf GenoCAE nsphs_ml_qt plink_1_9_unix plink_2_0_unix
+rm -f richel-sens2021565/*.zip
+rm -rf GenoCAE nsphs_ml_qt plink_1_9_unix plink_2_0_unix gcae gcaer
 rm -f *.log *.png
 rm -f 01_unzip_starter_zip.sh README.md 98_clean_bianca.sh
 rm nsphs_ml_qt.sif
