09062991b18e8ab0b694d3792b12c8226d25d7fc
richelbilderbeek <richel@richelbilderbeek.nl>
2022-03-17 17:01:28 +0100

    Use correct file name extensions

--- scripts_rackham/16_create_tidy_results.R
+++ scripts_rackham/16_create_tidy_results.R
@@ -30,9 +30,9 @@ if (length(unique_id) != 1) stop("'unique_id' must be 1 element")
 message("Parameters are valid")
 
 png_filename <- file.path(trainedmodeldir, paste0(unique_id, ".png"))
-csv_filename_for_mse <- file.path(trainedmodeldir, paste0(unique_id, "_mse.png"))
-csv_filename_for_fits <- file.path(trainedmodeldir, paste0(unique_id, "_fits.png"))
-csv_filename_for_r_squareds <- file.path(trainedmodeldir, paste0(unique_id, "_r_squareds.png"))
+csv_filename_for_mse <- file.path(trainedmodeldir, paste0(unique_id, "_mse.csv"))
+csv_filename_for_fits <- file.path(trainedmodeldir, paste0(unique_id, "_fits.csv"))
+csv_filename_for_r_squareds <- file.path(trainedmodeldir, paste0(unique_id, "_r_squareds.csv"))
 
 message("png_filename: ", png_filename)
 message("csv_filename_for_mse: ", csv_filename_for_mse)
