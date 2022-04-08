58f58eefaf77c0266be9ba2b06416df03e6f3673
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-08 12:17:18 +0200

    GHA runs scripts

--- scripts_local/20_start_issue_18.sh
+++ scripts_local/20_start_issue_18.sh
@@ -4,7 +4,7 @@
 #
 # Usage:
 #
-#   ./nsphs_ml_qt/scripts_local/20_start_issue_18.sh
+#   sbatch ./nsphs_ml_qt/scripts_rackham/20_start_issue_18.sh
 #
 #
 #SBATCH -A snic2021-22-624
@@ -20,14 +20,6 @@ echo "Starting time: $(date --iso-8601=seconds)"
 echo "Running on computer with HOSTNAME: $HOSTNAME"
 echo "Running at location $(pwd)"
 
-if [ ! -f gcae/gcae.sif ]; then
-  echo "'gcae/gcae.sif' file not found"
-  echo "Showing pwd:"
-  ls
-  echo "Showing content of the 'gcae' folder:"
-  cd gcae ; ls ; cd -
-  exit 42
-fi
 if [ ! -f gcaer/gcaer.sif ]; then
   echo "'gcaer/gcaer.sif' file not found"
   echo "Showing pwd:"
@@ -38,12 +30,15 @@ if [ ! -f gcaer/gcaer.sif ]; then
 fi
 
 gcae_experiment_params_filename=~/sim_data_issue_18/experiment_params.csv
-echo "gcae_experiment_params_filename: ${gcae_experiment_params_filename}"
 
-./nsphs_ml_qt/scripts_local/21_create_issue_18_data.sh   $gcae_experiment_params_filename
-./nsphs_ml_qt/scripts_rackham/25_run.sh                  $gcae_experiment_params_filename
-./nsphs_ml_qt/scripts_rackham/28_analyse.sh              $gcae_experiment_params_filename
-./nsphs_ml_qt/scripts_rackham/29_zip.sh                  $gcae_experiment_params_filename
+unique_id=$(echo $gcae_experiment_params_filename | egrep -o "issue_[[:digit:]]+")
+echo "gcae_experiment_params_filename: ${gcae_experiment_params_filename}"
+echo "unique_id: ${unique_id}"
+
+./nsphs_ml_qt/scripts_rackham/21_create_issue_18_params.sh $gcae_experiment_params_filename
+./nsphs_ml_qt/scripts_rackham/22_create_issue_18_data.sh   $gcae_experiment_params_filename
+./nsphs_ml_qt/scripts_rackham/25_run.sh                    $gcae_experiment_params_filename
+./nsphs_ml_qt/scripts_rackham/29_zip.sh                    $gcae_experiment_params_filename
 
 echo "End time: $(date --iso-8601=seconds)"
 echo "Duration: $SECONDS seconds"
