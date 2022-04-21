7764e90bfaca7df207243fa7fafbf457edeb842d
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-21 10:44:11 +0200

    Start of 'create_fake_bianca'

---
+++ R/create_fake_bianca.R
@@ -0,0 +1,6 @@
+#' Fake the environment on Bianca as realistic as possible,
+#' so the 'scripts_bianca' scripts can be run locally
+#' @export
+create_fake_bianca <- function() {
+
+}
---
+++ tests/testthat/test-create_fake_bianca.R
@@ -0,0 +1,3 @@
+test_that("use", {
+  expect_silent(create_fake_bianca())
+})
