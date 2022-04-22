54517f13a27cd68bc61a910672575ae1981cc4b2
richelbilderbeek <richel@richelbilderbeek.nl>
2022-04-22 10:44:05 +0200

    Cleaned up script

--- scripts_bianca/00_create_starter_zip.sh
+++ scripts_bianca/00_create_starter_zip.sh
@@ -43,18 +43,9 @@ cd ..
 rm -rf GenoCAE
 git clone https://github.com/richelbilderbeek/GenoCAE.git --branch Pheno --depth 1
 
-# Copy folders to the zip's root
-#cp -r ~/.local/share/plinkr/plink_1_9_unix plink_1_9_unix
-#cp -r ~/.local/share/plinkr/plink_2_0_unix plink_2_0_unix
-
 # Copy files for in the zip's root
 cp nsphs_ml_qt/scripts_bianca/README.md README.md
-cp nsphs_ml_qt/scripts_bianca/01_unzip_starter_zip.sh 01_unzip_starter_zip.sh
 cp nsphs_ml_qt/scripts_bianca/98_clean_bianca.sh 98_clean_bianca.sh
-
-# No need for gcae/gcae.sif anymore :-)
-#  plink_1_9_unix \
-#  plink_2_0_unix \
 
 # Use nsphs_ml_qt.sif in nsphs_ml_qt
 # Thanks https://silicondales.com/tutorials/linux/unix-linux-zip-contents-folder-excluding-certain-sub-folders/#Recursively_zip_a_directory_and_all_contents_-_excluding_one_subdirectory
@@ -62,16 +53,12 @@ zip -r \
   --must-match $zip_filename \
   nsphs_ml_qt/ \
   GenoCAE/ \
-  01_unzip_starter_zip.sh README.md 98_clean_bianca.sh \
+  README.md 98_clean_bianca.sh \
   --exclude nsphs_ml_qt/.git/**\* \
   --exclude nsphs_ml_qt/.Rproj.user/**\*
 
-# Remove folders from zip's root
-#rm -rf plink_1_9_unix
-#rm -rf plink_2_0_unix
-
 # Remove files for in the zip's root
-rm 01_unzip_starter_zip.sh README.md 98_clean_bianca.sh
+rm README.md 98_clean_bianca.sh
 
 # Place the zip where it is expected to be
 mv $zip_filename nsphs_ml_qt/$zip_filename
