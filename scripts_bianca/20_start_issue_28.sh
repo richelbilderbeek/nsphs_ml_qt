a1e08e2c6465e825077c4bbdf769f6c956438f17
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-02 10:19:09 +0200

    Scripts for #28

--- scripts_bianca/20_start_issue_28.sh
+++ scripts_bianca/20_start_issue_28.sh
@@ -15,28 +15,34 @@
 #SBATCH --partition core
 #SBATCH --ntasks 1
 #SBATCH --mem=16G
-#SBATCH --job-name=20_start_issue_5
-#SBATCH --output=20_start_issue_5.log
+#SBATCH --job-name=20_start_issue_28
+#SBATCH --output=20_start_issue_28.log
 
 SECONDS=0
 echo "Starting time: $(date --iso-8601=seconds)"
 echo "Running on computer with HOSTNAME: $HOSTNAME"
 echo "Running at location $(pwd)"
 
-gcae_experiment_params_filename=~/data_issue_5/experiment_params.csv
-unique_id=$(echo "$gcae_experiment_params_filename" | grep -E -o "issue_[[:digit:]]+")
-echo "gcae_experiment_params_filename: ${gcae_experiment_params_filename}"
-echo "unique_id: ${unique_id}"
 
-jobid_21=$(sbatch -A sens2021565                                  --output=21_create_${unique_id}_params.log ./nsphs_ml_qt/scripts_bianca/21_create_issue_5_params.sh "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
-jobid_22=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_21" --output=22_create_${unique_id}_data.log   ./nsphs_ml_qt/scripts_bianca/22_create_issue_5_data.sh   "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
-jobid_25=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_22" --output=25_run_${unique_id}.log           ./nsphs_ml_qt/scripts_rackham/25_run.sh                  "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
-jobid_29=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_25" --output=29_zip_${unique_id}.log           ./nsphs_ml_qt/scripts_bianca/29_zip.sh                   "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+for window_kb in 1 10 100 1000
+do
+  echo "window_kb: $window_kb"
 
-echo "jobid_21: ${jobid_21}"
-echo "jobid_22: ${jobid_22}"
-echo "jobid_25: ${jobid_25}"
-echo "jobid_29: ${jobid_29}"
+  gcae_experiment_params_filename=~/data_issue_28_$window_kb/experiment_params.csv
+  unique_id=$(echo "$gcae_experiment_params_filename" | grep -E -o "issue_[[:digit:]]+_[[:digit:]]+")
+  echo "gcae_experiment_params_filename: ${gcae_experiment_params_filename}"
+  echo "unique_id: ${unique_id}"
+
+  jobid_21=$(sbatch -A sens2021565                                  --output=21_create_${unique_id}_params.log ./nsphs_ml_qt/scripts_bianca/21_create_issue_28_params.sh "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+  jobid_22=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_21" --output=22_create_${unique_id}_data.log   ./nsphs_ml_qt/scripts_bianca/22_create_issue_28_data.sh   "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+  jobid_25=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_22" --output=25_run_${unique_id}.log           ./nsphs_ml_qt/scripts_rackham/25_run.sh                   "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+  jobid_29=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_25" --output=29_zip_${unique_id}.log           ./nsphs_ml_qt/scripts_bianca/29_zip.sh                    "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+
+  echo "jobid_21: ${jobid_21}"
+  echo "jobid_22: ${jobid_22}"
+  echo "jobid_25: ${jobid_25}"
+  echo "jobid_29: ${jobid_29}"
+done
 
 echo "End time: $(date --iso-8601=seconds)"
 echo "Duration: $SECONDS seconds"
