87def121dde98a9a1770540764f5bf17c0277886
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-02 15:12:05 +0200

    Always do a PLINK association, progress #30

---
+++ scripts_rackham/26_assoc_qt.R
@@ -0,0 +1,85 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+if (1 == 2) {
+  args <- "~/sim_data_issue_18/experiment_params.csv"
+}
+
+if (length(args) != 1) {
+  stop(
+    "Invalid number of arguments: must have 1 parameter: \n",
+    " \n",
+    "  1. gcae_experiment_params_filename \n",
+    " \n",
+    "Actual number of parameters: ", length(args), " \n",
+    "Parameters: {", paste0(args, collapse = ", "), "}"
+  )
+}
+
+gcae_experiment_params_filename <- args[1]
+message("gcae_experiment_params_filename: ", gcae_experiment_params_filename)
+testthat::expect_true(file.exists(gcae_experiment_params_filename))
+
+# Create the parameter file and dataset for #18
+matches <- stringr::str_match(
+  gcae_experiment_params_filename,
+  "^((.*)(issue_[:digit:]+)_([:digit:]+)/)experiment_params\\.csv"
+)
+message("matches: \n * ", paste0(matches, collapse = "\n * "))
+if (any(is.na(matches))) {
+  stop(
+    "no matches found for ",
+    "'gcae_experiment_params_filename': ", gcae_experiment_params_filename
+  )
+}
+unique_id <- matches[1, 4]
+message("unique_id: ", unique_id)
+datadir <- matches[1, 2]
+message("datadir: ", datadir)
+window_kb <- as.numeric(matches[1, 5])
+message("window_kb: ", window_kb)
+data <- basename(datadir)
+message("data: ", data)
+base_input_filename <- paste0(datadir, data)
+message("base_input_filename: ", base_input_filename)
+
+
+bed_filename <- paste0(base_input_filename, ".bed")
+message("bed_filename: ", bed_filename)
+
+bim_filename <- paste0(base_input_filename, ".bim")
+message("bim_filename: ", bim_filename)
+
+fam_filename <- paste0(base_input_filename, ".fam")
+message("fam_filename: ", fam_filename)
+
+phe_filename <- paste0(base_input_filename, ".phe")
+message("phe_filename: ", phe_filename)
+
+base_output_filename <- base_input_filename
+message("base_output_filename: ", base_output_filename)
+
+message("Do quantitative trait association")
+qassoc_filenames <- plinkr::assoc_qt(
+  assoc_qt_data = plinkr::create_assoc_qt_data(
+    data = plinkr::create_plink_bin_filenames(
+      bed_filename = bed_filename,
+      bim_filename = bim_filename,
+      fam_filename = fam_filename
+    ),
+    phenotype_data = plinkr::create_phenotype_data_filename(
+      phe_filename = phe_filename
+    )
+  ),
+  assoc_qt_params = plinkr::create_assoc_qt_params(
+    base_input_filename = base_input_filename,
+    base_output_filename = base_output_filename
+  )
+)
+message(
+  "qassoc_filenames$qassoc_filenames: \n * ",
+  paste0(qassoc_filenames$qassoc_filenames, collapse = "\n * ")
+)
+message(
+  "qassoc_filenames$log_filename: ",
+  qassoc_filenames$log_filename
+)
