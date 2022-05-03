815fe333bd712d28ec816abb006bcacb040147c3
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-03 12:05:54 +0200

    No use of 'superpops', fix #31

--- scripts_bianca/21_create_issue_29_params.R
+++ scripts_bianca/21_create_issue_29_params.R
@@ -51,7 +51,7 @@ message("base_input_filename: ", base_input_filename)
 gcae_setup <- gcaer::create_gcae_setup(
   datadir = datadir,
   data = data,
-  superpops = paste0(base_input_filename, "_labels.csv"),
+  superpops = "",
   model_id = "M1",
   train_opts_id = "ex3",
   data_opts_id = "b_0_4",
@@ -64,7 +64,7 @@ gcae_experiment_params <- gcaer::create_gcae_experiment_params(
   gcae_setup = gcae_setup,
   gcae_options = gcae_options,
   analyse_epochs = seq(10, 1000, by = 10),
-  metrics = "f1_score_3,f1_score_5,f1_score_7,f1_score_9,f1_score_11"
+  metrics = ""
 )
 gcaer::save_gcae_experiment_params(
   gcae_experiment_params = gcae_experiment_params,
