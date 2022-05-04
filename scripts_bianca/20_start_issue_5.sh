8d93d6de5d56fdd464582501a7d224610719e265
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-04 15:26:42 +0200

    Always plot the distribution of the phenotype input data, fix #33

--- scripts_bianca/20_start_issue_5.sh
+++ scripts_bianca/20_start_issue_5.sh
@@ -28,14 +28,16 @@ unique_id=$(echo "$gcae_experiment_params_filename" | grep -E -o "issue_[[:digit
 echo "gcae_experiment_params_filename: ${gcae_experiment_params_filename}"
 echo "unique_id: ${unique_id}"
 
-jobid_21=$(sbatch -A sens2021565                                              --output=21_create_"${unique_id}"_params.log ./nsphs_ml_qt/scripts_bianca/21_create_issue_5_params.sh "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
-jobid_22=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_21"             --output=22_create_"${unique_id}"_data.log   ./nsphs_ml_qt/scripts_bianca/22_create_issue_5_data.sh   "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
-jobid_25=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_22"             --output=25_run_"${unique_id}".log           ./nsphs_ml_qt/scripts_rackham/25_run.sh                  "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
-jobid_26=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_22"             --output=26_assoc_qt_"${unique_id}".log      ./nsphs_ml_qt/scripts_rackham/26_assoc_qt.sh              "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
-jobid_29=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_25":"$jobid_26" --output=29_zip_"${unique_id}".log           ./nsphs_ml_qt/scripts_bianca/29_zip.sh                    "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+jobid_21=$(sbatch -A sens2021565                                              --output=21_create_"${unique_id}"_params.log            ./nsphs_ml_qt/scripts_bianca/21_create_issue_5_params.sh    "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+jobid_22=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_21"             --output=22_create_"${unique_id}"_data.log              ./nsphs_ml_qt/scripts_bianca/22_create_issue_5_data.sh      "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+jobid_24=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_22"             --output=24_create_input_data_plots_"${unique_id}".log  ./nsphs_ml_qt/scripts_rackham/24_create_input_data_plots.sh "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+jobid_25=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_22"             --output=25_run_"${unique_id}".log                      ./nsphs_ml_qt/scripts_rackham/25_run.sh                     "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+jobid_26=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_22"             --output=26_assoc_qt_"${unique_id}".log                 ./nsphs_ml_qt/scripts_rackham/26_assoc_qt.sh                "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
+jobid_29=$(sbatch -A sens2021565 --dependency=afterok:"$jobid_25":"$jobid_26" --output=29_zip_"${unique_id}".log                      ./nsphs_ml_qt/scripts_bianca/29_zip.sh                      "$gcae_experiment_params_filename" | cut -d ' ' -f 4)
 
 echo "jobid_21: ${jobid_21}"
 echo "jobid_22: ${jobid_22}"
+echo "jobid_24: ${jobid_24}"
 echo "jobid_25: ${jobid_25}"
 echo "jobid_26: ${jobid_26}"
 echo "jobid_29: ${jobid_29}"
