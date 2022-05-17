26c330b2149d0dda05ce3a0a417afd9647c932eb
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-17 11:30:29 +0200

    Progress #45

---
+++ scripts_local/issue_45.R
@@ -0,0 +1,37 @@
+library(gcaer)
+
+gcae_options <- create_gcae_options("/opt/gcae_richel")
+gcae_experiment_params <- create_gcae_experiment_params(
+  gcae_options = gcae_options,
+  gcae_setup = create_test_gcae_setup(
+    superpops = "", # no labels
+  ),
+  analyse_epochs = c(1, 2),
+  metrics = "" # no metrics
+)
+
+for (model_id in get_gcae_model_ids(gcae_options = gcae_options)) {
+  for (pheno_model_id in get_gcae_pheno_model_ids(gcae_options = gcae_options)) {
+    gcae_experiment_params$trainedmodeldir <- paste0(
+      normalizePath(
+        gcaer::get_gcaer_tempfilename(),
+        paste0("test_model_", model_id, "_", pheno_model_id),
+        mustWork = FALSE
+      ),
+      "/"
+    )
+    gcae_experiment_params$gcae_setup$model_id <- model_id
+    gcae_experiment_params$gcae_setup$pheno_model_id <- pheno_model_id
+
+    passes <- FALSE
+    tryCatch({
+        do_gcae_experiment(
+          gcae_experiment_params = gcae_experiment_params
+        )
+        passes <- TRUE
+      }, error = function(e) {} # nolint do not care about the braces
+    )
+    message(paste(model_id, pheno_model_id, passes))
+  }
+}
+
