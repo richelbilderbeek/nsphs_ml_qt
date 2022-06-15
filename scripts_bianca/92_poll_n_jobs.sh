f75269a1efb924d153ff0c18e3ee0be8e3bd0076
richelbilderbeek <richel@richelbilderbeek.nl>
2022-06-15 10:15:22 +0200

    Made script more readable

--- scripts_bianca/92_poll_n_jobs.sh
+++ scripts_bianca/92_poll_n_jobs.sh
@@ -2,4 +2,11 @@
 #
 # Poll the number of jobs in the queue every minute for eternity
 #
-while true; do echo "$(date): $($(squeue -u "$USER" | wc --lines) - 1) jobs" ; sleep 60; done
+while true; 
+do 
+  date="$(date)"
+  n_jobs_plus_one="$(squeue -u $USER | wc --lines)"
+  n_jobs="$(${n_jobs_plus_one} - 1)"
+  echo "${date}: ${n_jobs} jobs"
+  sleep 60
+done
